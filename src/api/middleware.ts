a315ea539fe9749930908639d3e1a8e7f334763d
ka3vas <tyrnis@gmail.com>
2025-09-23 14:38:07 +0200

    middleware

---
+++ src/api/middleware.ts
@@ -0,0 +1,12 @@
+import { Request, Response, NextFunction } from "express";
+
+type Middleware = (req: Request, res: Response, next: NextFunction) => void;
+
+export const middlewareLogResponses: Middleware = (req, res, next) => {
+    res.on("finish", () => {
+        if (res.statusCode < 200 || res.statusCode >= 300) {
+            console.log(`[NON-OK] ${req.method} ${req.url} - Status: ${res.statusCode}`);
+        }
+    });
+    next();
+}
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
 import express from "express";
 
 import { handlerReadiness } from "./api/readiness.js";
+import { middlewareLogResponses } from "./api/middleware.js"
 
 const app = express();
 const PORT = 8080;
 
-app.use("/app", express.static("./src/app"));
+app.use("/app", middlewareLogResponses, express.static("./src/app"));
 
-app.get("/healthz", handlerReadiness);
+app.get("/healthz", middlewareLogResponses, handlerReadiness);
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
