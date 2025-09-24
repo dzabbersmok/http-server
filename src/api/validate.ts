4e0af35f7437ed3472389b1bc72383b88692809c
ka3vas <tyrnis@gmail.com>
2025-09-24 13:23:48 +0200

    validate refactor

--- src/api/validate.ts
+++ src/api/validate.ts
@@ -5,32 +5,15 @@ export const handlerValidateChirp: httpHandler = async (req, res) => {
     type parameters = {
         body: string;
     };
+    const params: parameters = req.body;
 
-    let body = "";
+    const maxChirpLength = 140;
+    if (params.body.length > maxChirpLength) {
+        respondWithError(res, 400, "Chirp is too long");
+        return;
+    }
 
-    req.on("data", chunk => {
-        body += chunk;
-    });
-
-    let params: parameters;
-
-    req.on("end", () => {
-        try {
-            params = JSON.parse(body);
-        } catch (error) {
-            respondWithError(res, 400, "Invalid JSON");
-            return;
-        }
-
-        const maxChirpLength = 140;
-
-        if (params.body.length > maxChirpLength) {
-            respondWithError(res, 400, "Chirp is too long");
-            return;
-        }
-
-        respondWithJSON(res, 200, {
-            valid: true,
-        });
+    respondWithJSON(res, 200, {
+        valid: true,
     });
 }
--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ import { handlerValidateChirp } from "./api/validate.js";
 const app = express();
 const PORT = 8080;
 
+app.use(express.json());
 app.use(middlewareLogResponses);
 app.use("/app", middlewareMetricsInc, express.static("./src/app"));
 
