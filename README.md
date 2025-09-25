601fd697b8d2019006506f79071f501b06c94723
ka3vas <tyrnis@gmail.com>
2025-09-25 15:16:17 +0200

    .env

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # http-server
 
 ## START
+Linux: sudo service postgresql start
+
+Create a new database chirpy:
+CREATE DATABASE chirpy;
+
+the new database:
+\c chirpy
--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,17 @@
+import process from "process";
+process.loadEnvFile("../.env");
+
 type APIConfig = {
   fileserverHits: number;
+  dbURL: string;
 };
 
+function envOrThrow(value: string | undefined, key: string): string {
+  if (!value) throw new Error(`Missing required env var: ${key}`);
+  return value;
+}
+
 export const config: APIConfig = {
-    fileserverHits: 0
+    fileserverHits: 0,
+    dbURL: envOrThrow(process.env.DB_URL, "DR_URL")
 }
